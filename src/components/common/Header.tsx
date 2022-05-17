f7a6579690fbb8ffee86dadff9a032da6687c94d
uhuikim <uhui94@naver.com>
2022-05-17 14:24:04 +0900

    feat: 폴더구조 변경 및 emotion 셋팅

---
+++ src/components/common/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { css } from '@emotion/react';
+
+const wrap = () => css`
+  background: black;
+`;
+
+const Header = () => {
+  return <div css={wrap}>ㅇㅇㅇ</div>;
+};
+
+export default Header;
